34680f30c99c2265fb2a6cb96742120d7c13767a
Ahmed SOUSSI <soussiahmed62@gmail.com>
2023-12-21 17:18:26 +0100

    logout button temporary

--- uber-clone/components/profile/Logout.tsx
+++ uber-clone/components/profile/Logout.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, Image } from 'react-native'
 import { auth } from '../../core/firebaseConfig'
 import { signOut } from 'firebase/auth'
 import React from 'react'
@@ -7,6 +7,7 @@ const Logout = () => {
   return (
     <TouchableOpacity style={styles.button} onPress={async () => await signOut(auth)}>
         <Text style={styles.buttonText}>Logout</Text>
+        <Image source={require('../../assets/logout.png')} style={{width: 20, height: 20, marginLeft: 10}}/>
     </TouchableOpacity>
 
   )
@@ -16,19 +17,30 @@ export default Logout
 
 const styles = StyleSheet.create({
     button: {
-        backgroundColor: '#fff',
+        backgroundColor: 'gray',
         width: '100%',
         height: 50,
         borderRadius: 10,
         borderWidth: 1,
-        borderColor: '#ccc',
-        color: 'white',
+        borderColor: 'gray',
         fontSize: 16,
         padding: 10,
         marginBottom: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: 'white', 
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+            width: 0,
+            height: 0
+        },
+        marginVertical: 30,
     }, 
     buttonText: {
-        color: '#000',
+        color: 'white',
         fontSize: 18,
+        fontWeight: 'bold',
     }
 })
